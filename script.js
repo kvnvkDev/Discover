1ac4ceb03ec1fd8a633c641706f58e3272934961
Kvn Rc <kvnvk3@gmail.com>
2023-07-18 07:56:30 -0300

    commit do projeto Discover

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function toggleBody(){
+    const body = document.body
+    body.classList.toggle("light")
+    body.style.transition = "all 0.5s"
+
+    const img = document.querySelector("img")
+
+    if (body.classList.contains("light")){
+        img.setAttribute("src", "./assets/avatar-light.png")
+    }else{
+        img.setAttribute("src", "./assets/avatar.png")
+    }
+}
